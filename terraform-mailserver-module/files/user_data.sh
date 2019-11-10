5a69df073cd4258d458ca67ac54d96b5c494c650
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2019-11-10 21:09:43 +0000

    set correct perms on mailbox host dir

--- terraform-mailserver-module/files/user_data.sh
+++ terraform-mailserver-module/files/user_data.sh
@@ -2,3 +2,6 @@
 
 # postfix is installed by default - remove it so that port 25 is freed up
 yum remove postfix
+
+# create mail directory and allow the vmail user (uid 500) to write to it
+mkdir -p /ecs/maildata && chown 500:500 /ecs/maildata
